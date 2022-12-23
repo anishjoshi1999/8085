a8bc3e8e4d324ff66435703ac9a39cb18b418045
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-12-23 07:35:19 +0545

    8085 programming

---
+++ OldQuestions/observe the output/observe_the_output.asm
@@ -0,0 +1,5 @@
+MVI A,45H 
+MVI B,66H 
+ADD B 
+ANI 00H
+HLT
---
+++ OldQuestions/observe the output/observe_the_output_2.asm
@@ -0,0 +1,9 @@
+;Observe the following program and write the
+;content of Accumulator, register B and flags 
+;after execution of each instruction. 
+;(assume initially all flags are reset)
+MVI A,59H 
+MVI B,67H
+ADD B
+ANI 3AH
+HLT
